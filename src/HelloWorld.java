1af097facc7a10b7e57570dc925023d0e2e65468
anhtuan <anhtuan@DESKTOP-M3B8JMO>
2016-12-01 02:30:26 +0700

    hello world

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public void sayHi(){
+		System.out.println("Hello World");
+	}
+	public static void main(String[] args) {
+	   HelloWorld hw = new HelloWorld();
+	   hw.sayHi();
+
+	}
+
+}
