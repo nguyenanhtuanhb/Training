86cbf320af73edb49fa627f342fd2319424cca7d
anhtuan <anhtuan@DESKTOP-M3B8JMO>
2016-12-01 02:33:05 +0700

    print name

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 
 	public void sayHi(){
 		System.out.println("Hello World");
+		System.out.println("Tuan");
 	}
 	public static void main(String[] args) {
 	   HelloWorld hw = new HelloWorld();
